2a916241a18386ce8d8c8e07433dc89d6cea494d
onkar koli <48193317+0nk4r@users.noreply.github.com>
2023-08-18 00:41:13 +0530

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "Po🐼",
-    image: "https://i.ibb.co/rcZ91pm/71c-JGg-Go6b-S-AC-SL1500.jpg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "https://i.ibb.co/HdDJPJJ/profil.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "Security Engineer",
     bio: "I hack things",
     email: "pxphacks@gmail.com",
